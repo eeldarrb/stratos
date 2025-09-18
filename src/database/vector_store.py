c48c1f7c76e0f7fa1b6b3a897d0713a09a85d17a
eeldarrb <167944058+eeldarrb@users.noreply.github.com>
2025-09-17 21:42:15 -0400

    feat(vector_store): replace _get_id_by_path with _get_item_by_path & create move_document
    - _get_id_by_path() returns tuple id, Item
    - refactor methods to use _get_item_by_path()

--- src/database/vector_store.py
+++ src/database/vector_store.py
@@ -21,6 +21,10 @@ class VectorStore:
             persist_directory=uri,
         )
 
+    def search(self, query, k=10):
+        results = self.vector_store.similarity_search_with_score(query=query, k=k)
+        return results
+
     def add_items(self, items: list[Item]):
         docs = []
         uuids = []
@@ -34,24 +38,55 @@ class VectorStore:
             )
         self.vector_store.add_documents(documents=docs, ids=uuids)
 
-    def delete_document(self, document_id):
-        self.vector_store.delete(ids=document_id)
-
-    def search(self, query, k=10):
-        results = self.vector_store.similarity_search_with_score(query=query, k=k)
-        return results
+    def move_document(self, old_path, new_path):
+        result = self._get_item_by_path(old_path)
+        if result:
+            doc_id, item = result
+            self.vector_store.update_document(
+                doc_id,
+                Document(
+                    page_content=item.text,
+                    metadata={"path": new_path, "mimetype": item.mimetype},
+                ),
+            )
 
     def update_document(self, file_path, new_item: Item):
-        doc_id = self._get_id_by_path(file_path)
-        if doc_id:
-            updated_doc = Document(
-                page_content=new_item.text,
-                metadata={"path": new_item.file_path, "mimetype": new_item.mimetype},
+        result = self._get_item_by_path(file_path)
+        if result:
+            doc_id, _ = result
+            self.vector_store.update_document(
+                doc_id,
+                Document(
+                    page_content=new_item.text,
+                    metadata={
+                        "path": new_item.file_path,
+                        "mimetype": new_item.mimetype,
+                    },
+                ),
             )
-            self.vector_store.update_document(doc_id, updated_doc)
 
-    def _get_id_by_path(self, file_path) -> str | None:
-        doc_ids = self.vector_store.get(where={"path": file_path}).get("ids")
-        if doc_ids and len(doc_ids) == 1:
-            return doc_ids[0]
+    def delete_document(self, file_path):
+        result = self._get_item_by_path(file_path)
+        if result:
+            doc_id, _ = result
+            self.vector_store.delete(ids=[doc_id])
+
+    def _get_item_by_path(self, file_path: str) -> tuple[str, Item] | None:
+        result = self.vector_store.get(where={"path": file_path})
+        ids = result.get("ids")
+        texts = result.get("documents")
+        metadatas = result.get("metadatas")
+
+        if ids and texts and metadatas and len(ids) == 1:
+            id = ids[0]
+            text = texts[0]
+            metadata = metadatas[0]
+
+            item = Item(
+                text=text,
+                file_path=metadata.get("path"),
+                mimetype=metadata.get("mimetype"),
+                source=metadata.get("source", ""),
+            )
+            return id, item
         return None
