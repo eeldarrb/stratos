981d8f934eb2b3eacf9920ced44e27a6ab4f59f8
eeldarrb <167944058+eeldarrb@users.noreply.github.com>
2025-10-05 14:46:34 -0400

    chore: add config example

---
+++ config.example.py
@@ -0,0 +1,11 @@
+# directory for sqlite db
+DATABASE_URI = "./data"
+
+# model names
+EMBEDDING_MODEL = "qwen3-embedding"
+IMAGE_MODEL = "gemma3"
+
+# absolute path of directories for listener_service to watch
+PATHS_TO_WATCH = [
+    "/path/to/dir/",
+]
