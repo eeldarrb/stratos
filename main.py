f7162d49a35bb3b93d0ce715044c99d75c86f903
eeldarrb <167944058+eeldarrb@users.noreply.github.com>
2025-10-09 21:27:25 -0400

    feat/create cli (#4)
    * chore: install typer
    
    * feat: create cli commands for starting listener service & querying
    
    - instead of directly running main.py to start listener, main.py
    refactored as cli entrypoint file
    
    * feat: create command for syncing all files

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
+import os
 import config
+import typer
 from src.file_service import FileService
 from src.listener_service import start_listener
 from src.file_store.vector_store import VectorStore
 
 
-def main():
-    vector_store = VectorStore(config.EMBEDDING_MODEL, config.DATABASE_URI)
-    file_service = FileService(vector_store)
+app = typer.Typer()
+vector_store = VectorStore(config.EMBEDDING_MODEL, config.DATABASE_URI)
+file_service = FileService(vector_store)
+
+
+@app.command()
+def start():
     start_listener(file_service, config.PATHS_TO_WATCH)
 
 
+@app.command()
+def query(query: str):
+    files = file_service.query_files(query)
+
+    for index, file_path in enumerate(files):
+        file_name = os.path.basename(file_path)
+        print(f"{index + 1}: {file_name}")
+
+
+@app.command()
+def sync():
+    for path in config.PATHS_TO_WATCH:
+        file_service.process_path(path)
+
+
 if __name__ == "__main__":
-    main()
+    app()
